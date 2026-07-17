f173d7f1bd6f44968eee0e82b67edefe0653131c
Sakatrz <sakatrz123@gmail.com>
2026-07-17 14:30:49 +0700

    tracking: require GPS fix quality before start/ready state

--- app/Http/Controllers/Api/TrackingController.php
+++ app/Http/Controllers/Api/TrackingController.php
@@ -268,7 +268,13 @@ class TrackingController extends Controller
         $lat = $vehicle->last_latitude;
         $lng = $vehicle->last_longitude;
         $hasCoordinates = $lat !== null && $lng !== null;
-        $gpsReady = $hasCoordinates;
+        $satellites = $vehicle->last_satellites;
+        $hdop = $vehicle->last_hdop;
+        $gpsReady = $hasCoordinates
+            && $satellites !== null
+            && $hdop !== null
+            && (int) $satellites >= 4
+            && (float) $hdop <= 5.0;
 
         // Hitung status IoT online/offline berdasarkan last_telemetry_received_at
         $lastReceived = $vehicle->last_telemetry_received_at;
@@ -287,8 +293,8 @@ class TrackingController extends Controller
             'heading_deg'     => $vehicle->last_heading_deg,
             'altitude'        => $vehicle->last_altitude,
             'accuracy_meters' => $vehicle->last_accuracy_meters,
-            'satellites'      => $vehicle->last_satellites,
-            'hdop'            => $vehicle->last_hdop,
+            'satellites'      => $satellites,
+            'hdop'            => $hdop,
             'baro_rel_alt_m'  => $vehicle->last_baro_rel_alt_m,
             'grade_pct'       => $vehicle->last_grade_pct,
             'gps_ready'       => $gpsReady,
